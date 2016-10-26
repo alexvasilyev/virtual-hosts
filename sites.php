442bd01c008c5deb7227e9b44b0aaed0b9d54131
Alexey Xiro Vasiljev <vasiljev.82@gmail.com>
2016-10-26 18:44:37 +0300

    Refactor code

--- sites.php
+++ sites.php
@@ -50,39 +50,64 @@ sort($confList);
  *   White 1;37
  **/
 echo "\033[1;33mList of virtual hosts:\033[0m" . PHP_EOL;
+
 foreach ($confList as $k => $v) {
+    $output = "";
     $confContents = file_get_contents($v);
 
-    // Server name
-    preg_match_all('/server_name\s+(?<s_name>[^;]+)/i', $confContents, $matches);
+    try {
+        $output = getServerName($confContents);
+    } catch (\Exception $e) {
+        $output .= "{$e->getMessage()}\033[0m(\033[1;30m{$v})";
+    }
+    echo $output . PHP_EOL;
+
+    // Root pwd
+    preg_match_all('/root\s+(?<s_root>[^;]+)/i', $confContents, $matches);
+
+    $match = $matches['s_root'];
+
+}
+
+/**
+ * @param $config
+ *
+ * @return string
+ *
+ * @throws Exception
+ */
+function getServerName($config)
+{
+    preg_match_all('/server_name\s+(?<s_name>[^;]+)/i', $config, $matches);
 
     $match = $matches['s_name'];
+    $output = "";
     switch (count($match)) {
         case 0:
-            echo "\033[0;31m empty server name\033[0m(\033[1;30m{$v})" . PHP_EOL;
+            throw new \Exception("\033[0;31mEmpty server name");
         case 1:
-            echo "\033[32m{$match[0]}\033[0m" . PHP_EOL;
+            $output .= "\033[32m{$match[0]}\033[0m";
             break;
         case 2:
-            echo "\033[0;32m{$match[1]}\033[0m (\033[0;33m{$match[0]}\033[0m)" . PHP_EOL;
+            $output .= "\033[0;32m{$match[1]}\033[0m (\033[0;33m{$match[0]}\033[0m)";
             break;
         case 2:
-            echo "\033[0;32m{$match[1]}\033[0m (\033[0;33m{$match[0]}\033[0m)" . PHP_EOL;
+            $output .= "\033[0;32m{$match[1]}\033[0m (\033[0;33m{$match[0]}\033[0m)";
             break;
         // more than 2 server names
         default:
-            echo "\033[0;32m{$match[0]}\033[0m (";
+            $output .= "\033[0;32m{$match[0]}\033[0m (";
             for ($i = 1; $i < count($match); $i++) {
-                echo "\033[0;33m{$match[$i]}";
+                $output .= "\033[0;33m{$match[$i]}";
                 if (($i + 1) !== count($match)) {
-                    echo ", ";
+                    $output .= ", ";
                 } else {
-                    echo "\033[0m)" . PHP_EOL;
+                    $output .= "\033[0m)";
                 }
             }
 
             break;
     }
 
-//    var_dump($matches['s_name']);
+    return $output;
 }
